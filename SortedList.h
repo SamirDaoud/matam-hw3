e4a7e7a181873afdd4e44900852a547d871f9c73
moham <agbariam@campus.technion.ac .il>
2025-01-04 12:59:30 +0200

    4.2.1

--- SortedList.h
+++ SortedList.h
@@ -13,31 +13,27 @@ namespace mtm {
         explicit Node(const T data, Node<T> *ptr = nullptr) :
                 data(data), next(ptr) {}
     };
-    class SortedList {
+        template <typename T>
+        class SortedList {
+            //head of the list and its tail.
+            Node<T>* head;
+            Node<T>* tail;
+            int size;
+            void deleteNodes()
+            {
+                Node<T>* temp=head;
+                while (temp)
+                {
+                    Node<T>* temp2= temp;
+                    temp = temp->next;
+                    delete temp2;
+                }
+
+                this->size=0;
+            };
+
     public:
-        /**
-         *
-         * the class should support the following public interface:
-         * if needed, use =defualt / =delete
-         *
-         * constructors and destructor:
-         * 1. SortedList() - creates an empty list.
-         * 2. copy constructor
-         * 3. operator= - assignment operator
-         * 4. ~SortedList() - destructor
-         *
-         * iterator:
-         * 5. class ConstIterator;
-         * 6. begin method
-         * 7. end method
-         *
-         * functions:
-         * 8. insert - inserts a new element to the list
-         * 9. remove - removes an element from the list
-         * 10. length - returns the number of elements in the list
-         * 11. filter - returns a new list with elements that satisfy a given condition
-         * 12. apply - returns a new list with elements that were modified by an operation
-         */
+
 
     };
 
